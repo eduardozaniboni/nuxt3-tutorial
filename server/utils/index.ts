c2ed6aeb16406ef478a44ed5231b64fe01bab10d
EduardoZaniboni <eduardowz.prog@gmail.com>
2024-10-28 21:38:57 -0300

    Acerto na conexão postgres

--- server/utils/index.ts
+++ server/utils/index.ts
@@ -1,12 +1,16 @@
 import pg from 'pg';
 
 export const postgresClient = () => {
+  // const client = new pg.Pool({
+  //   host: process.env.DB_HOST,
+  //   port: 5432,
+  //   database: process.env.DB_NAME,
+  //   user: process.env.DB_USER,
+  //   password: process.env.DB_PASS,
+  // });
+
   const client = new pg.Pool({
-    host: process.env.DB_HOST,
-    port: 5432,
-    database: process.env.DB_NAME,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
+    connectionString: process.env.DATABASE_URL,
   });
 
   return {
